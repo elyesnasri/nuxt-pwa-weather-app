e559fd9f549f3c15d10e1a6942efa28423d4906a
Elyes Nasri <elyes.nasri@gmail.com>
2019-10-16 17:23:09 +0200

    try to cache requests

---
+++ pages/cache.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Cache</h1>
+    <pre>{{ $data }}</pre>
+    <nuxt-link to="/about">About page</nuxt-link>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+let cachedData = null
+export default {
+  async asynData() {
+    if (cachedData) return cachedData
+    let { data } = await axios.get('https://api.myjson.com/bins/18pdl3')
+    cachedData = data
+    return data
+  }
+}
+</script>
