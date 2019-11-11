28fde0c6ee99e85f49b9efc55789760c2a25047d
elyesnasri <m.nasri@pure-apps.de>
2019-11-11 16:57:06 +0100

    cleaned up sw workbox

--- plugins/workbox-range-request.js
+++ plugins/workbox-range-request.js
@@ -4,9 +4,6 @@ console.log('[Workbox]: ' + precacheCacheName)
 workbox.routing.registerRoute(
   new RegExp('^https://api.openweathermap.org/data/2.5/weather'),
   new workbox.strategies.NetworkFirst({
-    // serve from cache if the network request is taking too long.
-    // networkTimeoutSeconds: 3,
-
     cacheName: 'api-cache',
     plugins: [
       new workbox.cacheableResponse.Plugin({
@@ -19,27 +16,7 @@ workbox.routing.registerRoute(
         maxAgeSeconds: 24 * 60 * 60,
         // Automatically cleanup if quota is exceeded.
         purgeOnQuotaError: true
-      }),
-      new workbox.broadcastUpdate.Plugin({
-        channelName: 'api-updates'
       })
     ]
   })
 )
-
-const updatesChannel = new BroadcastChannel('api-updates')
-updatesChannel.addEventListener('message', async (event) => {
-  const { cacheName, updatedUrl } = event.data.payload
-
-  // Do something with cacheName and updatedUrl.
-  // For example, get the cached content and update
-  // the content on the page.
-  // const cache = await caches.open(cacheName)
-  // const updatedResponse = await cache.match(updatedUrl)
-  // const updatedText = await updatedResponse.text()
-  // console.log(updatedResponse)
-  // console.log(updatedText)
-
-  console.log('data are updated in backround!')
-  alert('data was updated')
-})
