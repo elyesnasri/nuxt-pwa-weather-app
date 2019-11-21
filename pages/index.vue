b1dbaa30093596c91f754027627af33c6e197627
Elyes Nasri <elyes.nasri@gmail.com>
2019-11-21 16:38:14 +0100

    updated onesignal

--- pages/index.vue
+++ pages/index.vue
@@ -254,6 +254,17 @@ export default {
     }
     this.overlay = true
 
+    // oneSignal subscription to push notification
+    this.$OneSignal.push(() => {
+      this.$OneSignal.isPushNotificationsEnabled((isEnabled) => {
+        if (isEnabled) {
+          console.log('Push notifications are enabled!')
+        } else {
+          console.log('Push notifications are not enabled yet.')
+        }
+      })
+    })
+
     //PBS
     const status = await navigator.permissions.query({
       name: 'periodic-background-sync'
